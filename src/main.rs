c09900aa474641af017a2559c6514165c18f51e6
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2024-02-23 11:03:57 -0800

    Visualize sphere normal vectors by coloring them according to direction.

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,26 @@ use crate::ray::Ray;
 use crate::vec3::{Point3, Vec3, unit_vector, dot};
 use crate::color::Color;
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin() - center;
     let a = dot(ray.direction(), ray.direction());
     let b = 2.0 * dot(ray.direction(), oc);
     let c = dot(oc, oc) - radius * radius;
 
     let discriminant = b*b - 4.0*a*c;
-    return discriminant >= 0f64;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0*a);
+    }
 }
 
 fn ray_color(ray: &Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Color::new(1.0, 0.0, 0.0);
+    let sphere_center = Point3::new(0.0, 0.0, -1.0);
+    let t = hit_sphere(sphere_center, 0.5, ray);
+    if t > 0.0 {
+        let n = unit_vector(ray.at(t) - sphere_center);
+        return 0.5*Color::new(n.x+1.0, n.y+1.0, n.z+1.0);
     }
 
     let unit_direction = unit_vector(ray.direction());
